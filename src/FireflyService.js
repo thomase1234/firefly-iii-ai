eb39ca0e4af96a50abc2ac1d7c6e8195c5c2f2dd
Thomas - Laptop <thomas.elsen@rivy.org>
2025-11-25 19:04:37 +0100

    Refactor tag removal logging in transaction response to simplify debug output

--- src/FireflyService.js
+++ src/FireflyService.js
@@ -247,20 +247,10 @@ export default class FireflyService {
     const data = await response.json();
     const currentTags = data.data.attributes.transactions[0].tags || [];
 
-    this.#debugLog("Transaction response: only tags", { 
-      content: data,
-      tags: currentTags
-    });
-
     // Filtrer le tag à supprimer
-    const updatedTags = currentTags.filter(tag => tag.name !== tagName);
-    
-    this.#debugLog("Updated tags after removal", { 
-      originalTags: currentTags.map(t => t.name), 
-      updatedTags: updatedTags.map(t => t.name) 
-    });
+    const updatedTags = currentTags.filter(tag => tag !== tagName);
 
-    this.#debugLog("Thomas : Updated tags after removal", { 
+    this.#debugLog("Updated tags after removal", { 
       originalTags: currentTags, 
       updatedTags: updatedTags 
     });
